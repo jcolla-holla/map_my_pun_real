5d45abadf97d9e370afbb13dce9bc70714fefde4
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-02-19 12:38:32 -0500

    updated env setup to be accessible on heroku

--- frontend/util/maps_util.js
+++ frontend/util/maps_util.js
@@ -20,7 +20,7 @@ export function generateGoogleMapsAPIURL(coordsArr) {
     return `https://maps.googleapis.com/maps/api/staticmap?size=800x400&maptype=roadmap\&markers=size:mid%7Ccolor:red%${inputCoordsStr}&path=color:0x0000ff${pathInputStr}&key=AIzaSyBrpStMcZ1UILzkqi_eMI2pjTgo7SZmUl8`;
 }
 
-// return `https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%7CSan+Francisco,CA%7COakland,CA%7CSan+Jose,CA&key=AIzaSyBrpStMcZ1UILzkqi_eMI2pjTgo7SZmUl8`
+// return `https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%7CSan+Francisco,CA%7COakland,CA%7CSan+Jose,CA&key=${ENV['MAPS_API_KEY']}`
 //to update: color to be brand colors
 
     // 1)
@@ -46,10 +46,10 @@ export function generateGoogleMapsAPIURL(coordsArr) {
 
     //... 
     // TEMPLATE w fixed center:
-    // return `https://maps.googleapis.com/maps/api/staticmap?center=New York&size=800x500&maptype=roadmap\&markers=color:blue${inputCoordsStr}&path=color:0x0000ff|weight:5${inputCoordsStr}&key=AIzaSyBrpStMcZ1UILzkqi_eMI2pjTgo7SZmUl8`
+    // return `https://maps.googleapis.com/maps/api/staticmap?center=New York&size=800x500&maptype=roadmap\&markers=color:blue${inputCoordsStr}&path=color:0x0000ff|weight:5${inputCoordsStr}&key=${ENV['MAPS_API_KEY']}`
     
     // TEMPLATE w adjusted center:
-    // return `https://maps.googleapis.com/maps/api/staticmap?&size=800x500&maptype=roadmap\&markers=color:blue%${inputCoordsStr}&path=color:0x0000ff|weight:5${inputCoordsStr}&key=AIzaSyBrpStMcZ1UILzkqi_eMI2pjTgo7SZmUl8`
+    // return `https://maps.googleapis.com/maps/api/staticmap?&size=800x500&maptype=roadmap\&markers=color:blue%${inputCoordsStr}&path=color:0x0000ff|weight:5${inputCoordsStr}&key=${ENV['MAPS_API_KEY']}`
 // }
 
 
@@ -60,18 +60,18 @@ export function generateGoogleMapsAPIURL(coordsArr) {
 // }
 
 //EXAMPLE from google THAT WORKS for makers:
-// https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%7CSan+Francisco,CA%7COakland,CA%7CSan+Jose,CA&key=AIzaSyBrpStMcZ1UILzkqi_eMI2pjTgo7SZmUl8
+// https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%7CSan+Francisco,CA%7COakland,CA%7CSan+Jose,CA&key=${ENV['MAPS_API_KEY']}
 
 
 // EXAMPLE WORKS mine with replaced two MARKERS ONLY
-// https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%|59.900503,-135.478011|59.900603,-135.479011|&key=AIzaSyBrpStMcZ1UILzkqi_eMI2pjTgo7SZmUl8
+// https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%|59.900503,-135.478011|59.900603,-135.479011|&key=${ENV['MAPS_API_KEY']}
 
 
 
 //EXAMPLE FROM GOOGLE for path only (DOESNT WORK):
 
-// https://maps.googleapis.com/maps/api/staticmap?size=400x400&path = color: 0x0000ff | weight: 5 | 40.737102, -73.990318 | 40.749825, -73.987963 | 40.752946, -73.987384 | 40.755823, -73.986397|&key=AIzaSyBrpStMcZ1UILzkqi_eMI2pjTgo7SZmUl8
+// https://maps.googleapis.com/maps/api/staticmap?size=400x400&path = color: 0x0000ff | weight: 5 | 40.737102, -73.990318 | 40.749825, -73.987963 | 40.752946, -73.987384 | 40.755823, -73.986397|&key=${ENV['MAPS_API_KEY']}
 
 
 // EXAMPLE DOESNT WORK mine with replaced two MARKERS and short path with two points
-// https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%|59.900503,-135.478011|59.900603,-135.479011|&path=color:0x0000ff|weight:5|59.900503,-135.478011|59.900603,-135.479011|&key=AIzaSyBrpStMcZ1UILzkqi_eMI2pjTgo7SZmUl8
+// https://maps.googleapis.com/maps/api/staticmap?size=512x512&maptype=roadmap\&markers=size:mid%7Ccolor:red%|59.900503,-135.478011|59.900603,-135.479011|&path=color:0x0000ff|weight:5|59.900503,-135.478011|59.900603,-135.479011|&key=${ENV['MAPS_API_KEY']}
